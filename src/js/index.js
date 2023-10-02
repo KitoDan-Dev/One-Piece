96bd9f56ef13e6fe3470339a66d3102f2717847b
KITO.DAN <82036894+KitoDan-Dev@users.noreply.github.com>
2023-10-02 20:01:58 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,20 @@
 // Change all character settings when select the other buttom on menu, and show the new character settings..
 
-const buttons = document.querySelectorAll('.buttom') //pick the buttons for verify
+const buttons = document.querySelectorAll('.button') //pick the buttons for verify
 const characters = document.querySelectorAll('.character') //pick the characters for verify
 
-buttons.forEach((buttom, index) => {
-    buttom.addEventListener("click", () => { 
-        deselectbuttom ()
+buttons.forEach((button, index) => {
+    button.addEventListener("click", () => { 
+        deselectbutton ()
         deselectcharacter ()
-        buttom.classList.add("selected") //add "selected" to clicked buttom.
+        button.classList.add("selected") //add "selected" to clicked buttom.
         characters[index].classList.add("selected") //the index marks the character selected in characters list
     }) //Cath the click event and do an action.
 })  //select a unique buttom inside the buttons list.
 
-function deselectbuttom () {
-    const buttomselected = document.querySelector(".buttom.selected") //create a variable to pick the selected buttom.
-        buttomselected.classList.remove("selected") //remove the "selected" from the unclicked buttom.
+function deselectbutton () {
+    const buttonselected = document.querySelector(".button.selected") //create a variable to pick the selected buttom.
+        buttonselected.classList.remove("selected") //remove the "selected" from the unclicked buttom.
 }
 
 function deselectcharacter () {
